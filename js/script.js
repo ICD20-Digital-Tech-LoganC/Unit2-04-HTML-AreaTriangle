2b729a98bebee02aae0a939f15b8f21dabe0999d
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-03-28 00:01:32 +0000

    final product

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Logan Connors
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+"use strict";
+
+ //This function calculates area of rectangle.
+
+function calculate () {
+  // input
+  let base = parseInt(document.getElementById('base-of-triangle').value);
+  let height = parseInt(document.getElementById('height-of-triangle').value);
+
+  // process
+  let area = base * height / 2;
+
+  // output
+  document.getElementById('area').innerHTML = 'The area is: ' + area.toFixed(2) + ' cm²'
+
+}
